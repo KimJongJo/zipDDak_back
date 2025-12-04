080ac760cc712ffa3c5af84724b8a3e7f963a917
kyeongeunx <gyeongeh@gmail.com>
2025-12-05 08:47:43 +0900

    feat: orderList

--- zipddak/src/main/java/com/zipddak/mypage/dto/OrderListDto.java
+++ zipddak/src/main/java/com/zipddak/mypage/dto/OrderListDto.java
@@ -1,6 +1,7 @@
 package com.zipddak.mypage.dto;
 
 import java.sql.Date;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,9 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class OrderListDto {
-	private int orderIdx; // 주문 아이디
+	private Integer orderIdx; // 주문 아이디
 	private Date orderDate; // 주문날짜
 	private Boolean canCancel; // 취소 가능여부
 	private Boolean canReturn; // 교환환불 가능여부
-	private Object deliveryGroups[]; // 주문상품들
+	private List<DeliveryGroupsDto> deliveryGroups; // 주문상품들
 }
--- zipddak/src/main/java/com/zipddak/mypage/service/OrderService.java
+++ zipddak/src/main/java/com/zipddak/mypage/service/OrderService.java
@@ -1,9 +1,11 @@
 package com.zipddak.mypage.service;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.zipddak.mypage.dto.OrderListDto;
+import com.zipddak.util.PageInfo;
 
 public interface OrderService {
-	List<OrderListDto> getOrderList(String username) throws Exception;
+	List<OrderListDto> getOrderList(String username, PageInfo pageInfo, Date startDate, Date endDate) throws Exception;
 }
