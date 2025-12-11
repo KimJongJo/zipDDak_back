12b3456596eace6ec80b68260e0bcf5038ea72a8
leem jihae <leem.jihae@gmail.com>
2025-12-11 09:23:36 +0900

    1211 ljh 구글로그인, 카카오도 이메일을 준다네요

--- zipddak/src/main/java/com/zipddak/user/controller/UserController.java
+++ zipddak/src/main/java/com/zipddak/user/controller/UserController.java
@@ -1,25 +1,43 @@
 package com.zipddak.user.controller;
 
-import java.util.Map;
-
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.zipddak.auth.PrincipalDetails;
+import com.zipddak.dto.UserDto;
+import com.zipddak.entity.User;
+import com.zipddak.user.service.UserService;
 
 @RestController
 public class UserController {
 	
-	@PostMapping(value = "/user")
-	public ResponseEntity<Boolean> checkDoubleId() {
-		try {
-			
-			return ResponseEntity.ok().body(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.badRequest().body(false);
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	private UserService userService;
+	
+		@PostMapping("/login")
+		public ResponseEntity<UserDto> userInfo (@AuthenticationPrincipal PrincipalDetails principalDetails,
+				@RequestParam("fcmToken") String fcmToken) {
+			try {
+				User user = principalDetails.getUser();
+				user.setFcmToken(fcmToken);
+				UserDto userInfo = userService.login(user);
+//				alarmService.registFcmToken(user.getUsername(), fcmToken);
+				
+				return ResponseEntity.ok(userInfo);
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+				return ResponseEntity.badRequest().body(null);
+			}
 		}
-	}
 	
 
 }
