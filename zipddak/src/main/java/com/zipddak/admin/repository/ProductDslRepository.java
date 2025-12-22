2443a09138c9d4b65fa159c94f15b7837862aa74
KimJongJo <desk1614@gmail.com>
2025-12-22 17:52:34 +0900

    테스트중

--- zipddak/src/main/java/com/zipddak/admin/repository/ProductDslRepository.java
+++ zipddak/src/main/java/com/zipddak/admin/repository/ProductDslRepository.java
@@ -540,7 +540,9 @@ public class ProductDslRepository {
 	                    optionDto.setValue(o.getValue());
 	                    optionDto.setPrice(o.getPrice());
 	                    optionDto.setCount(o.getCount());
-	                    optionDto.setSalePrice(prod.getSalePrice()); // 필요시 계산
+	                    optionDto.setSalePrice(
+	                    	    prod.getSalePrice() != null ? prod.getSalePrice() : 0
+	                    	);
 	                    optionDto.setProductName(prod.getName());
 	                    optionDto.setPostCharge(prod.getPostCharge());
 	                    optionDto.setPostType(prod.getPostType());
